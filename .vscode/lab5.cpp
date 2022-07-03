d96281f12fe69d0bad0a16f16ea9e97fa0fe233c
harjodhsingh1 <harjodhsingh11@outlook.com>
2022-07-03 22:57:16 +0530

    Get answere for quetion

--- .vscode/lab5.cpp
+++ .vscode/lab5.cpp
@@ -2,24 +2,49 @@
 #include <fstream>
 
 using namespace std;
+
 int main() {
 	ifstream inStream;
-	inStream.open("ans.txt");
-	string ans;
+	
 	while (true) {
+		inStream.open("ans.txt");
 		cout << "Enter your next quetion";
 		string q;
 		cin >> q;
-		if (inStream >> ans) {
-			cout << ans << endl;
-		}
-		else{
-			inStream.close();
-			inStream.open("ans.txt");
-			inStream >> ans;
-			cout << ans << endl;
-		}
+		char ans[100];
 
+
+		int i = 0;
+		bool ignore = false;
+		inStream.get(ans[i++]);
+
+		while (!inStream.eof()) {
+			if (ans[i-1] =='\n') {
+				cout << ans << endl;
+				i = 0;
+			}
+			else if (ans[i - 1] == '#')
+			{
+				int x = 16; 
+				if (x >= 10)
+				{
+					ans[i - 1] = '1';
+					ans[i++] = (x - 10) + '0'; 
+				}
+				else
+				{
+					ans[i - 1] = x + '0';
+				}
+				ignore = true;
+			}
+			else if (ignore && ans[i - 1] == 'N')
+			{
+				i--;
+				inStream.get(ans[i++]);
+				continue;
+			}
+				inStream.get(ans[i++]);
+		}
 	}
 	return 0;
 }
